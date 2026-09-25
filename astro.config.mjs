71442119d546b88fbde97895d601b44f1f6baba4
Nio Febri <niofebriwulahadi111@gmail.com>
2026-09-25 16:19:20 +0700

    add sitemap integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
-
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://orion-entertainment-u9zq.vercel.app',
   integrations: [sitemap()],
-  // ...konfigurasi lain yang sudah ada, jangan dihapus
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
